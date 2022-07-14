83a09ea7f9bfb9c5d4836c0720d81953cecdf3db
Asen Georgiev <asen.connor@abv.bg>
2022-07-14 11:12:01 +0300

    Update handler.tf

--- terraform/handler.tf
+++ terraform/handler.tf
@@ -39,4 +39,12 @@ resource "aws_lambda_function" "terraform-lambda-demo" {
 resource "aws_lambda_function_url" "terraform-lambda-demo-url" {
   function_name      = aws_lambda_function.terraform-lambda-demo.function_name
   authorization_type = "AWS_IAM"
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["*"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["keep-alive", "date"]
+    max_age           = 86400
+  }
 }
