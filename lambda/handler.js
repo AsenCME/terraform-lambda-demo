65aa2f8a90d69d061a08a4162c380a1fa82a38b3
Asen Georgiev <asen.connor@abv.bg>
2022-07-15 22:13:44 +0300

    Update handler.js

--- lambda/handler.js
+++ lambda/handler.js
@@ -4,6 +4,6 @@ module.exports.handler = async () => {
     return {
         statusCode: 200,
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ message: "All Korrekt", number: random.int(0,100) })
+        body: JSON.stringify({ message: "Works!", number: random.int(0,100) })
     }
 }
