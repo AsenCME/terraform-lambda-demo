e4008f6bb35ee0aceb3a207bac819907c6e3b9cd
Asen Georgiev <asen.connor@abv.bg>
2022-07-15 21:41:46 +0300

    Update handler.tf

--- terraform/handler.tf
+++ terraform/handler.tf
@@ -4,8 +4,7 @@ resource "null_resource" "lambda_dependencies" {
   }
 
   triggers = {
-    package = sha256(file("${path.root}/../lambda/package.json"))
-    node = sha256(file("${path.root}/../lambda/handler.js"))
+    index = sha256(file("${path.root}/../lambda/handler.js"))
   }
 }
 
